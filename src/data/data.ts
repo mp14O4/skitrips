a1185061596ac8fbd0d590a880501237e6d44aac
Merlin Pfeifer <merlin.pfeifer@hs-augsburg.de>
2026-01-23 20:41:09 +0100

    interfaces

---
+++ src/data/data.ts
@@ -0,0 +1,47 @@
+export interface Season {
+  trips: Trip[];
+  passes: SeasonPass[];
+  services: SkiService[];
+}
+
+export interface SeasonPass {
+  name: string;
+  price: number;
+}
+
+
+export interface Trip {
+  start: Date;
+  // TODO these get generated automatically from the initial destination + trip length
+  skiDays: SkiDay[];
+
+  destination: string;
+  description: string;
+  people: string;
+
+  regularPrice: number; // TODO
+  actualPrice: number;
+}
+
+export interface SkiDay {
+  ski: Ski[];
+  destinationOverride?: string; // TODO how can we best design this?
+}
+
+export interface SkiService {
+  ski: Ski;
+  date: Date;
+}
+
+export interface Ski {
+  id: string;
+  name: string;
+}
+
+// description:
+//  home page has two groups: ski trips and skis
+//  trips group has a dropdown on the top to select the season
+//  i can add a trip
+//  in the creation page for a trip, i can input basic data except SkiDays.
+//  instead, there is a number input for the amount of days
+//  when i create a trip, the SkiDays are created automatically from the amount.
